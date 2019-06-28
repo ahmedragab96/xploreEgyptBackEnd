1d3dc51e4f95dd6c85cba4c683ca409631de4cfa
ahmedragab96 <hamadar1996@gmail.com>
2019-06-28 19:35:06 +0200

    fix deployment issue

--- src/app/services/hotels/hotels.service.ts
+++ src/app/services/hotels/hotels.service.ts
@@ -10,21 +10,21 @@ export class HotelsService {
   constructor(private http: HttpClient) { }
 
   GetAllHotels() {
-    return this.http.get('http://localhost:3000/hotels/getall');
+    return this.http.get('https://immense-cove-87813.herokuapp.com/hotels/getall');
   }
 
   getHotelByID(id) {
-    return this.http.get('http://localhost:3000/hotels/getById?id=' + id);
+    return this.http.get('https://immense-cove-87813.herokuapp.com/hotels/getById?id=' + id);
   }
 
   addHotel(hotelDetails) {
-    return this.http.post('http://localhost:3000/hotels/addhotel', hotelDetails).subscribe(data => {
+    return this.http.post('https://immense-cove-87813.herokuapp.com/hotels/addhotel', hotelDetails).subscribe(data => {
       console.log('Hotel has been added successfully ', data);
     },
       error => { console.log('Error', error); });
   }
 
   deleteHotel (id) {
-    return this.http.delete(`http://localhost:3000/hotels/delete/:id=${id}`);
+    return this.http.delete(`https://immense-cove-87813.herokuapp.com/hotels/delete/:id=${id}`);
   }
 }
